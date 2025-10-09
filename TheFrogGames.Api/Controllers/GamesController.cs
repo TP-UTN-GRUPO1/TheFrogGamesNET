68708da3905419655e297199e07cb37d44a57ec3
QuimeyAlejo <qalopez2000@gmail.com>
2025-10-09 17:37:43 -0300

    Added GameController, GameService and new tables migration

---
+++ TheFrogGames.Api/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using Contract.User.Request;
+using Microsoft.AspNetCore.Mvc;
+using TheFrogGames.Application.Service;
+using TheFrogGames.Contracts.Game.Request;
+
+namespace TheFrogGames.Api.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly IGameService _gameServices;
+        public GamesController(IGameService gamesServices)
+        {
+            _gameServices = gamesServices;
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] CreateGameRequest game)
+        {
+            var isCreated = _gameServices.Create(game);
+
+            if (!isCreated)
+            {
+                return Conflict("Error al crear el producto");
+            }
+
+            return Ok("Usuario creado");
+        }
+    }
+}
